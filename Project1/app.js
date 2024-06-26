32d197deb921d54a5c281665a521dab6a2d626ca
jaekwan3033 <jaekwan3033@gmail.com>
2024-06-25 21:50:48 -0500

    updating js

--- Project1/app.js
+++ Project1/app.js
@@ -1,5 +1,9 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 // For function called by addEventListener,
 // event could be entered as argument, which JS
@@ -8,8 +12,23 @@ const onLoginSubmit = (event) => {
     // preventDefault stops default behavior. 
     // In this case is refreshing when submitting info.
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+// Changing class and text of greeting 
+const paintGreetings = (username) => {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
