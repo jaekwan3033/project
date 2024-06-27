91cb4799c6fe5bee319f88e6138e54e618afdaba
jaekwan3033 <jaekwan3033@gmail.com>
2024-06-26 21:35:34 -0500

    adding new js and pictures

---
+++ Project1/js/background.js
@@ -0,0 +1,8 @@
+const images = [
+    "img/image1.jpg",
+    "img/image2.jpg",
+    "img/image3.jpg"
+];
+
+const chosenIamge = images[Math.floor(Math.random()*images.length)];
+console.log(chosenIamge);
