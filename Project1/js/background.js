4cbb0ceb4eef770eae7eb3617e09e0175448fd15
jaekwan3033 <jaekwan3033@gmail.com>
2024-07-01 21:29:47 -0500

    changing background, and image

--- Project1/js/background.js
+++ Project1/js/background.js
@@ -1,16 +1,12 @@
 // Array of name of background images
 const images = [
-    "image1.jpg",
-    "image2.jpg",
-    "image3.jpg"
+    "img/image1.jpg",
+    "img/image2.jpg",
+    "img/image3.jpg"
 ];
 
 // Choosing images randomly from array of images
-const chosenIamge = images[Math.floor(Math.random()*images.length)];
+const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-// Making a <img> tag
-const bgImage = document.createElement("img");
-// Setting src attribute as chosen random images
-bgImage.src=`img/${chosenIamge}`;
-// appending <img tag> to HTML
-document.body.appendChild(bgImage);
+// applying back
+document.body.style.backgroundImage=`url(${chosenImage})`;
